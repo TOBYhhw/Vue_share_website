7e32db10e002765ba8e277c2cd7d9f37629ca9a9
TOBYhhw <1505994122@qq.com>
2022-06-29 00:50:35 +0800

    减少了请求响应时间

--- src/request/http.js
+++ src/request/http.js
@@ -2,14 +2,14 @@ import axios from "axios"
 export function myaxios(axiosConfig) {
     const service = axios.create({
         baseURL: "/api",
-        timeout: 90000
+        timeout: 10000
     })
     return service(axiosConfig)
 }
 export function myaxios2(axiosConfig) {
     const service = axios.create({
         baseURL: "/api",
-        timeout: 90000,
+        timeout: 10000,
         headers: {
             token: window.sessionStorage.getItem("token")
         }
